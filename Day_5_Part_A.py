8510ca7cb5f7104baf5f8c75c584ffab951f45e8
Myles Stubbs <jrstubbies@gmail.com>
2024-12-15 18:39:04 +1300

    modified:   Day_5_Part_A.py

--- Day_5_Part_A.py
+++ Day_5_Part_A.py
@@ -1,33 +1,34 @@
-from itertools import pairwise
-
-
-input_dict = {}
 rules = []
-# Read the contents of the file. First and second part of input is separated by double '\n' char
-with open('Day_5_Input.txt', 'r') as file:
-    rules_input, updates = file.read().strip().split("\n\n")
 
-    for line in rules_input.split("\n"):
-        x, y = line.split("|")
-        rules.append((int(x), int(y)))
+# Open the input and separate the two parts of the input using doube '\n'. 
+with open("Day_5_Input.txt", 'r') as file:
+    rule_input, updates = file.read().strip().split("\n\n")
     
-    # Store the values in the second part of the input as a list of ints for each line
+    # Then to get the rules, look at the first part and separate x, y using '|' delimeter.
+    for line in rule_input.split("\n"):
+        a, b = line.split("|")
+        rules.append((int(a), int(b)))
+
+    # The second part of input is comma separated string of nums
     updates = [list(map(int, line.split(","))) for line in updates.split("\n")]
 
-# CONVERT THIS INTO A FUNCTION -> IS CALCULATING SOMETHING IT SHOULDNT ????
-order = {}
-total = 0
-test = 0
-for u in updates:
-    for i, num in enumerate(u):
-        order[num] = i
+# function to check if update line follows the rules, if so return the middle num
+def check_rules(update):
+    idx = {}
+    for i, num in enumerate(update):
+        idx[num] = i
     
-    for x, y in rules:
-        if (x in order and y in order) and (not order[x] < order[y]):
-            test += 1
-            # take the sum of all MIDDLE numbers for each valid line
-            total += u[len(u) // 2] 
+    for a, b in rules:
+        if a in idx and b in idx and not idx[a] < idx[b]:
+            return False, 0
+        
+    return True, update[len(update) // 2]
 
+# Take the sum of all the middle nums for each of the valid input lines
+total = 0
+for u in updates:
+    valid, mid = check_rules(u)
+    if valid:
+        total += mid
 
 print(total)
-# 10813    == TOO HIGH
